0757ad4b454d1710eab9c5ebbacd921c3baf8f62
eddgest <gereon.steinwachs@ericsson.com>
2022-08-16 07:55:40 +0200

    comments on prices added

--- stromverbrauch.py
+++ stromverbrauch.py
@@ -76,13 +76,13 @@ if args.preis == None:
     if args.table == 'wasser':
         args.preis = 1.8032               # 1.9963 (von Rechnung)
     elif args.table == 'stromsonst':
-        # change on 1.5.2021: 0.2683
-        args.preis = 0.2651               # 22,41 (von Rechnung)
+        # change on 1.5.2021: 0.2683      # laut letzter Preisänderung (5/2021)
+        args.preis = 0.2651               # 22,41 (von letzter Rechnung)
         # change on 1.11.2022
         #args.preis = 0.3176
     elif args.table == 'waermepumpe':
-        # change on 1.5.2021: 0.2191
-        args.preis = 0.2123               # 17,48 (von Rechnung)
+        # change on 1.5.2021: 0.2191      # laut letzter Preisänderung (5/2021);
+        args.preis = 0.2123               # 17,48 (von letzter Rechnung)
         # change on 1.11.2022
         #args.preis = 0.2683
 
